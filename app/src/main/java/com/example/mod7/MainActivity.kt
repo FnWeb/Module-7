0096748894ba51211b8db330f04b19a74ee1aa90
FnPngn AlexandroHIV <disturbedelk@gmail.com>
2022-04-28 20:28:08 +0700

    Atomic types + type conversion

--- app/src/main/java/com/example/mod7/MainActivity.kt
+++ app/src/main/java/com/example/mod7/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mod7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inspector.PropertyReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +12,9 @@ class MainActivity : AppCompatActivity() {
 }
 
 interface Expr{
-    fun run(): Any{
+    fun run(): Variable{
         return when(this){
-            is Variable -> this.retrieve()
+            is Variable -> this
             is Command -> this.execute()
             else -> throw Exception("Undefined run behavior for $this")
         }
@@ -25,43 +26,138 @@ interface Command : Expr{
 }
 
 interface Variable : Expr{
-    open fun retrieve(): Any
+//    open fun retrieve(): Any
     open fun set(to: Any)
+    open operator fun invoke(): Any //Retrieve atomic value
 }
 
-class Integer(init : Any) : Variable{
+class IntegerType(init : Any) : Variable{
     private var value: Int = 0;
     init{
         set(init)
     }
 
-    override fun retrieve(): Integer{
-        return this
-    }
+//    override fun retrieve(): IntegerType{
+//        return this
+//    }
 
     override fun set(to: Any) {
-        when(to){
-            is Int -> value = to
+        value = when(to){
+            is Int -> to
             is String -> TODO("Requires string parser")
-            is Double -> value = round(to).toInt()
+            is Double -> kotlin.math.round(to).toInt()
+            is IntegerType -> to.value
             else -> throw Exception("Variable type mismatch: $to can not be converted to Int")
         }
     }
 
-    operator fun plus(operand: Integer): Integer{
-        return Integer(this.value + operand.value)
+    operator fun plus(operand: IntegerType): IntegerType{
+        return IntegerType(this.value + operand.value)
     }
-    operator fun minus(operand: Integer): Integer{
-        return Integer(this.value - operand.value)
+    operator fun minus(operand: IntegerType): IntegerType{
+        return IntegerType(this.value - operand.value)
     }
-    operator fun div(operand: Integer): Integer{
-        return Integer(this.value / operand.value)
+    operator fun div(operand: IntegerType): IntegerType{
+        return IntegerType(this.value / operand.value)
     }
-    operator fun rem(operand: Integer): Integer{
-        return Integer(this.value % operand.value)
+    operator fun rem(operand: IntegerType): IntegerType{
+        return IntegerType(this.value % operand.value)
     }
-    operator fun invoke(): Int{
+    override operator fun invoke(): Int{
         return value
+    }
+}
+
+class DoubleType(init : Any) : Variable{
+    private var value: Double = 0.0;
+    init{
+        set(init)
+    }
+
+//    override fun retrieve(): DoubleType{
+//        return this
+//    }
+
+    override fun set(to: Any) {
+        value = when(to){
+            is Int -> to.toDouble()
+            is String -> TODO("Requires string parser")
+            is Double -> to
+            is IntegerType -> to().toDouble()
+            else -> throw Exception("Variable type mismatch: $to can not be converted to Int")
+        }
+    }
+
+    operator fun plus(operand: DoubleType): DoubleType{
+        return DoubleType(this.value + operand.value)
+    }
+    operator fun minus(operand: DoubleType): DoubleType{
+        return DoubleType(this.value - operand.value)
+    }
+    operator fun div(operand: DoubleType): DoubleType{
+        return DoubleType(this.value / operand.value)
+    }
+    operator fun rem(operand: DoubleType): DoubleType{
+        return DoubleType(this.value % operand.value)
+    }
+    override operator fun invoke(): Double{
+        return value
+    }
+}
+
+class BoolType(init : Any) : Variable{
+    private var value: Boolean = false;
+    init{
+        set(init)
+    }
+
+//    override fun retrieve(): BoolType{
+//        return this
+//    }
+
+    override fun set(to: Any) {
+        value = when(to){
+            is Int -> to>0
+            is String -> to!=""
+            is Double -> to>0
+            else -> throw Exception("Variable type mismatch: $to can not be used to initialize a Boolean")
+        }
+    }
+
+    operator fun plus(operand: BoolType): BoolType{
+        return BoolType(this.value || operand.value)
+    }
+    operator fun rem(operand: BoolType): BoolType{
+        return BoolType(this.value && !operand.value)
+    }
+    override operator fun invoke(): Boolean{
+        return value
+    }
+}
+
+fun castType(from: Expr, to: Expr): Variable{
+    if(from is Command || to is Command)
+        throw Exception("Unable to typecast expressions")
+    return when(to){
+        is IntegerType -> when(from) {
+            is IntegerType -> from
+            is BoolType -> IntegerType(from())
+            is DoubleType -> IntegerType(from().toInt())
+            else -> throw IllegalArgumentException("Unable to typecast $from to $to")
+        }
+        is DoubleType -> when(from) {
+            is IntegerType -> DoubleType(from())
+            is BoolType -> DoubleType(from())
+            is DoubleType -> from
+            else -> throw IllegalArgumentException("Unable to typecast $from to $to")
+        }
+        is BoolType -> when (from){
+            is IntegerType -> BoolType(from()!=0)
+            is BoolType -> from
+            is DoubleType -> BoolType(from()!=0.0)
+            else -> throw IllegalArgumentException("Unable to typecast $from to $to")
+        }
+        else -> throw Exception("Unable to typecast $from to $to")
     }
 }
 
@@ -75,12 +171,20 @@ class Add(left: Expr, right: Expr): Command{
         TODO("ugh")
     }
 
+    //Unable to perform typecast without branching because the only operator for type comparison is "is"
     override fun execute(): Variable {
-        val operandL = left.run()
-        val operandR = right.run()
-        if(operandL is Integer && operandR is Integer) {
-            result = operandL+operandL
-        } else throw Exception("IDK what is $operandL or $operandR")
+        var operandL: Variable = left.run()
+        var operandR: Variable = right.run()
+        operandL = if(operandL is BoolType && operandR is BoolType)
+            IntegerType(operandR()) else castType(operandL, operandR)
+
+        operandR = castType(operandR, operandL)
+
+        if(operandL is IntegerType &&  operandR is IntegerType)
+            result = operandL + operandR
+        if(operandL is DoubleType &&  operandR is DoubleType)
+            result = operandL + operandR
+
         if(result == null)
             throw Exception("Addition error: $operandL + $operandR is null")
         return result!!
