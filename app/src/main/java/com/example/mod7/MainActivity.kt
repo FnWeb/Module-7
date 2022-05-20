cb7fa72f75b0254fdafb65c138a330d42b44bb61
FnPngn AlexandroHIV <disturbedelk@gmail.com>
2022-05-20 15:23:34 +0700

    Block selection

--- app/src/main/java/com/example/mod7/MainActivity.kt
+++ app/src/main/java/com/example/mod7/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mod7
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
 import android.view.View
 import android.view.inspector.PropertyReader
@@ -10,11 +12,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
 import androidx.core.view.allViews
 import java.lang.Exception
 import com.example.mod7.databinding.ActivityMainBinding
 
 const val variableNameRegex = "[a-zA-Z]+(\\d|[a-zA-Z])*"
+var selectedBlock = -1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityMainBinding
@@ -42,5 +46,24 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "INVALID VARIABEL", Toast.LENGTH_SHORT).show()
             (view as TextView).text=""
         }
+    }
+    fun onBlockClicked(view: View){
+        val id = view.id
+        if(selectedBlock>=0){
+            program.blockViewManager.blocks[selectedBlock].first.findViewById<TextView>(R.id.blockTitle).setTextColor(ContextCompat.getColor(this, R.color.orange))
+        }
+        val selectionColor = ContextCompat.getColor(this, R.color.white)
+        program.blockViewManager.blocks.apply {
+            val index = indexOf(find { it.first.id == id })
+
+            if(index>=0){
+                selectedBlock=index
+                this[index].first.findViewById<TextView>(R.id.blockTitle).setTextColor(selectionColor)
+            } else{
+                selectedBlock=-1
+                program.exceptionHandler.throwCritical("Error retrieving selected block ID")
+            }
+        }
+        Toast.makeText(this, "$selectedBlock", Toast.LENGTH_SHORT).show()
     }
 }
